f97693e8765fce767d7980d5152ca7b95a5000f5
Javier Farre <jfarre90@gmail.com>
2019-02-21 13:15:01 +0000

    added the javascript to retrieve bitcoin price from coindesk

---
+++ javascript.js
@@ -0,0 +1,21 @@
+var btn = document.querySelector("button");
+var price = document.querySelector("#price");
+var currency = "USD"
+
+function getPrice(){
+  var XHR = new XMLHttpRequest();
+        
+        XHR.onreadystatechange = function (){
+            if(XHR.readyState ==4 && XHR.status ==200) {
+                var value = JSON.parse(XHR.responseText).bpi[currency].rate;
+           
+                price.innerHTML = value + " " + currency;
+            }
+        };
+        XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
+        XHR.send();
+}
+    // listen for clicks
+    btn.addEventListener("click", getPrice());
+
+window.onload(getPrice());
