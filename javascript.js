fc6db12d3bd10b1c8c2b5a8cc8fae919fbe9c2f1
Javier Farre <jfarre90@gmail.com>
2019-02-21 13:24:16 +0000

    corrections to window.onload

--- javascript.js
+++ javascript.js
@@ -3,19 +3,19 @@ var price = document.querySelector("#price");
 var currency = "USD"
 
 function getPrice(){
-  var XHR = new XMLHttpRequest();
+    var XHR = new XMLHttpRequest();
         
-        XHR.onreadystatechange = function (){
-            if(XHR.readyState ==4 && XHR.status ==200) {
-                var value = JSON.parse(XHR.responseText).bpi[currency].rate;
-           
-                price.innerHTML = value + " " + currency;
-            }
-        };
-        XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
-        XHR.send();
+    XHR.onreadystatechange = function (){
+        if(XHR.readyState ==4 && XHR.status ==200) {
+            var value = JSON.parse(XHR.responseText).bpi[currency].rate;
+            price.innerHTML = value + " " + currency;
+        }
+    };
+    XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
+    XHR.send();
 }
-    // listen for clicks
-    btn.addEventListener("click", getPrice());
 
-window.onload(getPrice());
+// listen for clicks
+btn.addEventListener("click", getPrice());
+
+window.onload = getPrice;
